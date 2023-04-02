8f063123802a027ea6d0773f2de8d1a38c12b183
Muhammad Sulton <musulton@gmail.com>
2023-04-02 20:58:50 +0700

    update codecov script

---
+++ src/pages/404.test.tsx
@@ -0,0 +1,9 @@
+import {render} from "@testing-library/react";
+
+import NotFound from "@/pages/404";
+
+it("should render NotFound correctly", () => {
+    const {container} = render(<NotFound />)
+
+    expect(container.firstChild).toMatchSnapshot();
+})
