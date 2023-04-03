922d20d6473866fe80d288c87251ea750f149096
Muhammad Sulton <musulton@gmail.com>
2023-04-03 10:36:50 +0700

    add title and metadata to all page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
+import Head from "next/head";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
+      <>
+          <Head>
+              <title>Pokemon site</title>
+              <meta name='description' content='Pokémon are mysterious creatures with many secrets.' />
+          </Head>
+          <main className={inter.className}>
+              <Component {...pageProps} />
+          </main>
+      </>
   )
 }
